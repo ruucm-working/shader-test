dd2f321fe0d2aef41f8f00e235226dc66ff3f4f9
ruucm <ruucm.a@gmail.com>
2022-05-01 23:28:31 +0900

    Init

---
+++ src/components/canvas/Shader/shaders/index.js
@@ -0,0 +1,4 @@
+import * as test1 from './test1'
+import * as test2 from './test2'
+
+export { test1, test2 }
---
+++ src/store.js
@@ -0,0 +1,46 @@
+import * as qs from 'query-string'
+import create from 'zustand'
+import { combine } from 'zustand/middleware'
+
+// without embedMode
+// it renders without the dom & other gradient controls at first, and add it after the first updateGradientState() excuted.
+
+export const useGradientStore = create((set) => ({
+  ...parseState(),
+}))
+
+export const usePropertyStore = create((set) => ({
+  hoverState: 0,
+  zoomOut: false,
+  toggleAxis: false,
+}))
+
+export const updateGradientState = (querystate) => {
+  const state = parseState(querystate)
+  useGradientStore.setState(state, true)
+}
+
+// defaultGradient could be replaced by window.location.search
+function parseState(search = '?shader=test1') {
+  return qs.parse(search, {
+    parseNumbers: true,
+    parseBooleans: true,
+    arrayFormat: 'index',
+  })
+}
+
+export const useDomStore = create(() => {
+  return { dom: null }
+})
+
+// export const useUIStore = create(
+//   combine(
+//     { activePreset: initialActivePreset, mode: 'full', loadingPercentage: 0 },
+//     (set) => ({
+//       setActivePreset: (by: number) => set((state) => ({ activePreset: by })),
+//       setMode: (data: any) => set((state) => ({ ...state, mode: data })),
+//       setLoadingPercentage: (data: any) =>
+//         set((state) => ({ ...state, loadingPercentage: data })),
+//     })
+//   )
+// )
